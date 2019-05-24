bdcf182e3477e2d59879f05a75d3257f059002fe
Steven McGrath <mcgrath.steven7@gmail.com>
2019-05-24 11:09:30 -0400

    added shift, reverse, sort, splice, unshift

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-Array.prototype.famFill = function(value, startIndex, endIndex) {
-  let newArray = [...this];
-  return newArray.fill(value, startIndex, endIndex);
-};
+module.exports = [
+  (Array.prototype.famFill = function(value, startIndex, endIndex) {
+    let newArray = [...this];
+    return newArray.fill(value, startIndex, endIndex);
+  }),
 
-Array.prototype.famPop = function() {
-  let newArray = [...this];
-  const poppedItem = newArray.pop();
-  return {
-    arr: newArray,
-    item: poppedItem,
-  };
-};
+  (Array.prototype.famPop = function() {
+    let newArray = [...this];
+    const poppedItem = newArray.pop();
+    return {
+      arr: newArray,
+      item: poppedItem,
+    };
+  }),
 
-Array.prototype.famPush = function(element) {
-  let newArray = [...this];
-  let newLength = newArray.push(element);
-  return {
-    arr: newArray,
-    len: newLength,
-  };
-};
-let arr = [1, 2, 3, 4];
+  (Array.prototype.famPush = function(element) {
+    let newArray = [...this];
+    let newLength = newArray.push(element);
+    return {
+      arr: newArray,
+      len: newLength,
+    };
+  }),
 
-let newArr = arr.famPush(3);
+  (Array.prototype.famReverse = function() {
+    let newArray = [...this];
+    return newArray.reverse();
+  }),
 
-console.log(arr);
-console.log(newArr.len, newArr.arr);
+  (Array.prototype.famShift = function() {
+    let newArray = [...this];
+    let shiftedItem = newArray.shift();
+    return {
+      arr: newArray,
+      item: shiftedItem,
+      len: newArray.length,
+    };
+  }),
+
+  (Array.prototype.famSort = function(sortFunction) {
+    let newArray = [...this];
+    return newArray.sort(sortFunction);
+  }),
+
+  (Array.prototype.famSplice = function(
+    startIndex,
+    deleteCount,
+    itemsToSpliceIn
+  ) {
+    let newArray = [...this];
+    const itemArgs = [].concat(...Array.from(arguments)).slice(2);
+    newArray.splice(startIndex, deleteCount, ...itemArgs);
+    return newArray;
+  }),
+
+  (Array.prototype.famUnshift = function(itemsToAdd) {
+    let newArray = [...this];
+    const argArray = [].concat(...Array.from(arguments)).famReverse();
+    if (argArray.length > 0) {
+      argArray.forEach(arg => {
+        newArray.unshift(arg);
+      });
+    }
+    return {
+      arr: newArray,
+      len: newArray.length,
+    };
+  }),
+];
